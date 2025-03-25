6e2d5b9ea213e26047e4f36efb824d2fd1f7f63c
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-03-25 20:29:11 -0300

    video playsinline

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -33,6 +33,7 @@ const HeroSection = () => {
             autoPlay
             loop
             muted
+            playsInline
             className="rounded-lg w-full md:w-1/2 border border-lime-700 shadow-sm shadow-lime-400 mx-2 my-4 pointer-events-none"
           >
             <source src={video1} type="video/mp4" />
@@ -42,6 +43,7 @@ const HeroSection = () => {
             autoPlay
             loop
             muted
+            playsInline
             className="rounded-lg w-full md:w-1/2 border border-lime-700 shadow-sm shadow-lime-400  mx-2 my-4 pointer-events-none"
           >
             <source src={video2} type="video/mp4" />
